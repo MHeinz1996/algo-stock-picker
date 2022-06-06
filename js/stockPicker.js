8f7e014ad3309797a4c3109d971bbaec789ec370
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-06 12:42:22 -0500

    Completed algo

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,19 @@
 exports.picker = function(prices) {
+    let largestProfit = 0
 
+    for(i=0; i<prices.length; i++) {
+        for(j=1; j<prices.length; j++) {
+            if(i<j) {
+                let profit = prices[j] - prices[i]
+                    if(profit > largestProfit) {
+                        largestProfit = profit
+                        buySell = [i, j]
+                    }
+            }
+        }
+    }
+
+    return buySell
 }
+
+// console.log(exports.picker([17,3,6,9,15,8,6,1,10]))
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,12 @@
 def picker(prices):
-    pass
+    largest_profit = 0
+
+    for i in range(len(prices)):
+        for j in range(1, len(prices)):
+            if i<j:
+                profit = prices[j]-prices[i]
+                if profit > largest_profit:
+                    largest_profit = profit
+                    buy_sell = [i, j]
+
+    return buy_sell
